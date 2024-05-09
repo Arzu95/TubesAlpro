8b41cce383eb89526db84a528219cb35d90deaa8
Aria Lent <kaqyword2304@gmail.com>
2024-05-09 21:11:31 +0700

    tambah functional

--- main.go
+++ main.go
@@ -2,7 +2,27 @@ package main
 
 import "fmt"
 
+type pasien struct {
+	nama, umur, asal, gologan_riwayat_penyakit string
+	tanggalLahir int
+}
+
+func cetakDataTerurut() {
+
+}
+
+func pilihRole() {
+
+}
+
+func daftarPasien() {
+
+}
+
+func cariData() {
+
+}
+
 func main(){
-	var name = "Muhammad Arzu Kirana Berutu"
-	fmt.Println(len(name))
+	
 }
